44ebeb3b695db067e00f99dd60f0afebdfe29467
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2024-03-14 10:03:29 -0300

    feature: ajustando tamanho do game e naves

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
-const canvasWidth = canvas.width = 800;
+const canvasWidth = canvas.width = 400;
 const canvasHeight = canvas.height = 600;
 // Variável de estado para controlar se o jogo está em andamento
 let jogoEmAndamento = true;
+// Variável para armazenar a pontuação
+let pontuacao = 0;
 
 // Classe para representar a nave do jogador
 class PlayerShip {
@@ -36,7 +38,12 @@ class PlayerShip {
 
     draw() {
         ctx.fillStyle = 'blue';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.beginPath();
+        ctx.moveTo(this.x + this.width / 2, this.y); // Inicia o caminho no topo central do triângulo
+        ctx.lineTo(this.x + this.width, this.y + this.height); // Desenha uma linha até o canto inferior direito
+        ctx.lineTo(this.x, this.y + this.height); // Desenha uma linha até o canto inferior esquerdo
+        ctx.closePath(); // Fecha o caminho
+        ctx.fill(); // Preenche o triângulo com a cor definida
 
         // Desenhar os tiros
         this.shots.forEach(shot => shot.draw());
@@ -69,32 +76,32 @@ class Shot {
 
 // Array para armazenar os quadrados na barra superior
 const quadrados = [
-    { x: 100, y: 10, width: 40, height: 40, isVisible: true },
-    { x: 200, y: 10, width: 40, height: 40, isVisible: true },
-    { x: 300, y: 10, width: 40, height: 40, isVisible: true },
-    { x: 400, y: 10, width: 40, height: 40, isVisible: true },
-    { x: 500, y: 10, width: 40, height: 40, isVisible: true },
-    { x: 600, y: 10, width: 40, height: 40, isVisible: true },
+    { x: 40, y: 10, width: 20, height: 40, isVisible: true },
+    { x: 100, y: 10, width: 20, height: 40, isVisible: true },
+    { x: 160, y: 10, width: 20, height: 40, isVisible: true },
+    { x: 210, y: 10, width: 20, height: 40, isVisible: true },
+    { x: 270, y: 10, width: 20, height: 40, isVisible: true },
+    { x: 320, y: 10, width: 20, height: 40, isVisible: true },
 
-    { x: 150, y: 70, width: 40, height: 40, isVisible: true },
-    { x: 250, y: 70, width: 40, height: 40, isVisible: true },
-    { x: 350, y: 70, width: 40, height: 40, isVisible: true },
-    { x: 450, y: 70, width: 40, height: 40, isVisible: true },
-    { x: 550, y: 70, width: 40, height: 40, isVisible: true },
+    // { x: 15, y: 70, width: 20, height: 40, isVisible: true },
+    // { x: 25, y: 70, width: 20, height: 40, isVisible: true },
+    // { x: 35, y: 70, width: 20, height: 40, isVisible: true },
+    // { x: 45, y: 70, width: 20, height: 40, isVisible: true },
+    // { x: 55, y: 70, width: 20, height: 40, isVisible: true },
 
-    { x: 200, y: 140, width: 40, height: 40, isVisible: true },
-    { x: 300, y: 140, width: 40, height: 40, isVisible: true },
-    { x: 400, y: 140, width: 40, height: 40, isVisible: true },
-    { x: 500, y: 140, width: 40, height: 40, isVisible: true },
+    // { x: 20, y: 140, width: 20, height: 40, isVisible: true },
+    // { x: 30, y: 140, width: 20, height: 40, isVisible: true },
+    // { x: 40, y: 140, width: 20, height: 40, isVisible: true },
+    // { x: 50, y: 140, width: 20, height: 40, isVisible: true },
+    // { x: 25, y: 220, width: 20, height: 40, isVisible: true },
 
-    { x: 250, y: 220, width: 40, height: 40, isVisible: true },
-    { x: 350, y: 220, width: 40, height: 40, isVisible: true },
-    { x: 450, y: 220, width: 40, height: 40, isVisible: true },
+    // { x: 35, y: 220, width: 20, height: 40, isVisible: true },
+    // { x: 45, y: 220, width: 20, height: 40, isVisible: true },
 
-    { x: 300, y: 290, width: 40, height: 40, isVisible: true },
-    { x: 400, y: 290, width: 40, height: 40, isVisible: true },
+    // { x: 30, y: 290, width: 20, height: 40, isVisible: true },
+    // { x: 40, y: 290, width: 20, height: 40, isVisible: true },
 
-    { x: 350, y: 360, width: 40, height: 40, isVisible: true },
+    // { x: 35, y: 360, width: 20, height: 40, isVisible: true },
 
 ];
 
@@ -102,10 +109,12 @@ const quadrados = [
 function gameLoop() {
     if (!jogoEmAndamento) return;
     requestAnimationFrame(gameLoop);
+    updateEnemies();
     update();
     render();
 }
 
+let ultimoAcertoTempo = Date.now();
 // Função de atualização do estado do jogo
 function update() {
     // Atualizar a posição dos tiros
@@ -125,6 +134,10 @@ function update() {
                     shot.colidiu = true; // Marcar que o tiro colidiu com um quadrado
                     shot.isVisible = false; // Marcar o tiro como invisível
                     quadrado.isVisible = false;
+                    const agora = Date.now();
+                    const diferencaTempo = agora - ultimoAcertoTempo;
+                    ultimoAcertoTempo = agora;
+                    score(diferencaTempo);
                     break; // Interromper o loop assim que uma colisão for detectada
                 }
             }
@@ -146,8 +159,34 @@ function update() {
         if (resposta === 'sim') {
             quadrados.forEach(quadrado => quadrado.isVisible = true);
             jogoEmAndamento = true;
+            pontuacao = 0;
+            shot.ded();
         }
     }
+}
+
+// Variável para controlar a direção do movimento dos inimigos
+let direcaoInimigos = 1; // 1 para mover para a direita, -1 para mover para a esquerda
+
+// Função para atualizar a posição dos inimigos
+function updateEnemies() {
+    // Verificar se os inimigos chegaram às bordas da tela
+    let chegouNaBorda = false;
+    quadrados.forEach(quadrado => {
+        if (quadrado.x + quadrado.width >= canvasWidth || quadrado.x <= 0) {
+            chegouNaBorda = true;
+        }
+    });
+
+    // Se algum inimigo chegou à borda, mudar a direção do movimento
+    if (chegouNaBorda) {
+        direcaoInimigos *= -1; // Inverter a direção
+    }
+
+    // Atualizar a posição dos inimigos com base na direção do movimento
+    quadrados.forEach(quadrado => {
+        quadrado.x += direcaoInimigos * 2; // Mover os inimigos para a direita ou esquerda
+    });
 }
 
 
@@ -158,14 +197,35 @@ function render() {
     // Desenhar quadrados
     quadrados.forEach(quadrado => {
         if (quadrado.isVisible) {
+            // Desenhar triângulo azul apontando para baixo
             ctx.fillStyle = 'red';
-            ctx.fillRect(quadrado.x, quadrado.y, quadrado.width, quadrado.height);
+            ctx.beginPath();
+            ctx.moveTo(quadrado.x, quadrado.y); // Inicia o caminho no canto superior esquerdo
+            ctx.lineTo(quadrado.x + quadrado.width, quadrado.y); // Desenha uma linha até o canto superior direito
+            ctx.lineTo(quadrado.x + quadrado.width / 2, quadrado.y + quadrado.height); // Desenha uma linha até a base do triângulo (canto inferior central)
+            ctx.closePath(); // Fecha o caminho
+            ctx.fill(); // Preenche o triângulo com a cor definida
+        }
+    });
+    // Desenhar tiros
+    playerShip.shots.forEach(shot => {
+        if (shot.isVisible) {
+            ctx.fillStyle = 'yellow';
+            ctx.fillRect(shot.x, shot.y, shot.width, shot.height);
         }
     });
     // Desenhar nave
     playerShip.draw();
 }
 
+const divScore = document.getElementById("score");
+function score(pontosNovos) {
+    const desconto = Math.floor(pontosNovos/95) ;
+    if(desconto <= 100) {
+        pontuacao += 100 - desconto;
+    }
+    divScore.innerHTML = 'Score: ' + pontuacao;
+}
 
 // Instanciar a nave do jogador
 const playerShip = new PlayerShip();
